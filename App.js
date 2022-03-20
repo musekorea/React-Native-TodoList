cc0e382909c960c6ccc2ce1656826857fbbb3b94
musekorea <musekorea@hanmail.net>
2022-03-20 14:51:04 +0800

    header

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { app_color } from "./theme.js";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Text style={styles.header_title}>To Do</Text>
+				<Text style={styles.header_title}>To Buy</Text>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: app_color.background,
+		alignItems: "center",
+	},
+	header: {
+		flexDirection: "row",
+		justifyContent: "space-around",
+		width: "100%",
+		padding: "10px",
+	},
+	header_title: {
+		fontSize: "40px",
+		fontWeight: 600,
+		color: app_color.deactivate,
+	},
 });
---
+++ theme.js
@@ -0,0 +1,5 @@
+export const app_color = {
+	background: "black",
+	deactivate: "#3a3d40", //Gray
+	fontDefaultColor: "white",
+};
